fae49d6567ef11c16c60c4718da54dddb069e85e
Victor Castro <victor.castro1@bp.com>
2025-03-02 21:15:56 -0300

    production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv'
 import { z } from 'zod'
 
-if (process.env.NODE_ENV == 'test') {
+if (process.env.NODE_ENV === 'test') {
   config({ path: '.env.test' })
 } else {
   config({ path: '.env' })
@@ -9,7 +9,9 @@ if (process.env.NODE_ENV == 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
 })
 
 // export const env = envSchema.parse(process.env)
